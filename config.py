eda3a17b575c1c738469c60f2806ba719f670bc8
alhamami <89762635+alhamami@users.noreply.github.com>
2022-05-06 05:19:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgres://zbnlxtmlovaaus:808b773adf253882212303c1a23964fa5b5b621ab55ff177d53036ed6cf02d51@ec2-52-1-20-236.compute-1.amazonaws.com:5432/d1j3kdp6fvuehv/Analysight'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:o0kARjWrSUxAcv61DyEM@containers-us-west-50.railway.app:5714/railway'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
